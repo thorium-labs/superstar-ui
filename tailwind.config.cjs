083242073cb9a43c07ec27ce2254d8fda961242c
Kyaris <iris.ledesma.27@gmail.com>
2022-11-20 01:44:39 +0100

    add base ss color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,19 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "ss-orange-100": "#FEF5D7",
+        "ss-orange-200": "#FEE9B0",
+        "ss-orange-300": "#FDD989",
+        "ss-orange-400": "#FBC96B",
+        "ss-orange-500": "#FAAF3B",
+        "ss-orange-600": "#D78C2B",
+        "ss-orange-700": "#B36C1D",
+        "ss-orange-800": "#905012",
+        "ss-orange-900": "#773B0B",
+      }
+    },
   },
   plugins: [],
 }
