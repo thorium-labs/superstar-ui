29de7d8f4ffde74eb4de30cde3561f7ad772cb72
Kyaris <iris.ledesma.27@gmail.com>
2022-11-27 16:26:10 +0100

    update about

--- src/modules/About/About.tsx
+++ src/modules/About/About.tsx
@@ -1,27 +1,50 @@
 import React from "react";
-import { Logo } from "../../components/Icons";
-import Earth from "../../components/Icons/Earth";
-import Jupiter from "../../components/Icons/Jupiter";
-import Mars from "../../components/Icons/Mars";
-import Mercury from "../../components/Icons/Mercury";
-import Neptune from "../../components/Icons/Neptune";
-import Pluton from "../../components/Icons/Pluton";
-import Saturn from "../../components/Icons/Saturn";
-import Uranus from "../../components/Icons/Uranus";
-import Venus from "../../components/Icons/Venus";
 
 const About: React.FC = () => {
   return (
     <div className="relative">
-      <Mercury className="h-[3rem] w-[3rem] absolute top-[35rem] left-[-1rem] z-20 animate-floating" />
-      <Venus className="h-[5rem] w-[5rem] absolute top-[-4rem] left-[15rem]" />
-      <Earth className="h-[6rem] w-[6rem] absolute bottom-[5rem] left-[4rem] animate-floating" />
-      <Mars className="h-[5rem] w-[5rem] absolute top-[5rem] right-[-4rem] " />
-      <Jupiter className="h-[9rem] w-[9rem] absolute top-[-6rem] right-[-2rem] animate-floating" />
-      <Saturn className="h-[8rem] w-[8rem] absolute top-[-5rem] z-20 animate-floating" />
-      <Uranus className="h-[7rem] w-[5rem] absolute bottom-[7rem] right-[-2rem] z-20 animate-floating" />
-      <Neptune className="h-[5rem] w-[5rem] absolute bottom-[18rem] left-[-4rem] " />
-      <Pluton className="h-[3rem] w-[3rem] absolute bottom-[5rem] right-[5rem] animate-floating" />
+      <img
+        src="assets/orange-ball.png"
+        alt="ball"
+        className="h-[3rem] w-[3rem] absolute top-[35rem] left-[-1rem] z-20 animate-floating"
+      />
+      <img
+        src="assets/orange-ball.png"
+        alt="ball"
+        className="h-[4rem] w-[4rem] absolute top-[-3rem] left-[15rem]"
+      />
+      <img
+        src="assets/orange-ball.png"
+        alt="ball"
+        className="h-[6rem] w-[6rem] absolute bottom-[5rem] left-[4rem] animate-floating"
+      />
+      <img
+        src="assets/orange-ball.png"
+        alt="ball"
+        className="h-[5rem] w-[5rem] absolute top-[5rem] right-[-4rem] z-20 "
+      />
+      <img
+        src="assets/ss-orange-ball.png"
+        alt="ball"
+        className="h-[9rem] w-[9rem] absolute top-[-6rem] right-[-2rem] animate-floating"
+      />
+      <div className="absolute top-[-8rem] left-[-1rem] z-20">
+        <img
+          src="assets/launch.png"
+          alt="ball"
+          className="h-[13rem] w-[13rem]  animate-floating "
+        />
+      </div>
+      <img
+        src="assets/ss-orange-ball.png"
+        alt="ball"
+        className="h-[3rem] w-[3rem] absolute bottom-[5rem] right-[5rem] animate-floating"
+      />
+      <img
+        src="assets/orange-ball.png"
+        alt="ball"
+        className="h-[7rem] w-[7rem] animate-floating absolute bottom-[7rem] right-[-2rem] z-20"
+      />
       <div className="flex flex-col gap-4 bg-stone-700/20 backdrop-blur rounded-xl relative my-36">
         <div className="grid grid-cols-2 py-20 px-8 gap-8">
           <h1 className="text-6xl">Why we are better than others?</h1>
@@ -98,7 +121,7 @@ const About: React.FC = () => {
             waiting to be unleashed.
           </p>
         </div>
-        <div className="absolute text-stone-800 bottom-[-4rem] bg-gradient-to-tr from-ss-orange-500 to-orange-500 flex gap-12 px-12 py-8 rounded-lg left-0 right-0 mx-auto max-w-fit">
+        <div className="absolute text-stone-100/80 bottom-[-4rem] bg-gradient-to-tr from-ss-orange-500 to-orange-500 flex gap-12 px-12 py-8 rounded-lg left-0 right-0 mx-auto max-w-fit">
           <div className="flex flex-col items-center justify-center">
             <h3 className="text-4xl">23</h3>
             <p className="uppercase font-extrabold text-sm">winners /month</p>
