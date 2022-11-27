93ed31a825a4afa672af566b9a9e8600e6be87a5
Kyaris <iris.ledesma.27@gmail.com>
2022-11-27 16:26:28 +0100

    update ball

--- src/components/StarBall/StarBall.tsx
+++ src/components/StarBall/StarBall.tsx
@@ -8,8 +8,9 @@ interface Props {
 
 const StarBall: React.FC<Props> = ({ num }) => {
   return (
-    <div className="relative h-17 w-17 flex items-center justify-center rounded-full ">
-      <Star className="ball-planet" />
+    <div className="relative w-[4rem] min-h-[4rem] flex items-center justify-center rounded-full ">
+      <img src="/assets/stone-ball.png" className="w-[4rem] min-h-[4rem]" />
+      <Star className="ball-planet absolute" />
       <span className="absolute text-stone-800 text-xl font-bold top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ">
         {num}
       </span>
