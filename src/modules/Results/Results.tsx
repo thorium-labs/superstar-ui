29a852d67c6312ddace26b1b10da61bf7d7d053c
Kyaris <iris.ledesma.27@gmail.com>
2022-12-04 07:02:29 +0100

    add animation on results

--- src/modules/Results/Results.tsx
+++ src/modules/Results/Results.tsx
@@ -1,69 +1,47 @@
-import clsx from "clsx";
-import React, { useState } from "react";
-import { GradientButton } from "../../components/Buttons";
-import { useWallet } from "../../providers/WalletProvider";
-import MyTickets from "./components/MyTickets";
-import Statistics from "./components/Statistics";
-import Transactions from "./components/Transactions";
+import clsx from 'clsx';
+import React, { useState } from 'react';
+import { GradientButton } from '../../components/Buttons';
+import { useWallet } from '../../providers/WalletProvider';
+import MyTickets from './components/MyTickets';
+import RecentDraws from './components/RecentDraws';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const Results: React.FC = () => {
-  const { connectWallet, address } = useWallet();
-  const [currentTab, setCurrenTab] = useState<
-    "statistics" | "my-tickets" | "transactions"
-  >("statistics");
+  const [currentTab, setCurrenTab] = useState<'recent-draws' | 'my-tickets'>('recent-draws');
 
-  if (!address) {
+  const TabButton = (key: 'recent-draws' | 'my-tickets') => {
     return (
-      <div className="min-h-[10rem] w-full flex items-center justify-center flex-col gap-4">
-        <p className="text-stone-400">
-          Please, connect your wallet to see your information
-        </p>
-        <GradientButton className="text-2xl" onClick={connectWallet}>
-          Connect
-        </GradientButton>
-      </div>
+      <button key={key} className={clsx('uppercase', key === currentTab ? 'selected' : '')} onClick={() => setCurrenTab(key)}>
+        {key.replace('-', ' ')}
+        {key === currentTab ? (
+          <motion.div
+            className="underline bg-gradient-to-bl from-ss-orange-500/80 to-orange-500/80 h-[2px] w-full rounded-md"
+            layoutId="underline"
+          />
+        ) : null}
+      </button>
     );
-  }
+  };
 
   return (
-    <div className="w-full flex flex-col items-center justify-center">
-      <div className="flex gap-12 text-xl font-semibold">
-        <button
-          className={clsx(
-            "uppercase",
-            currentTab === "statistics" &&
-              "text-transparent bg-clip-text bg-gradient-to-bl from-ss-orange-500/80 to-orange-500/80"
-          )}
-          onClick={() => setCurrenTab("statistics")}
-        >
-          Statistics
-        </button>
-        <button
-          className={clsx(
-            "uppercase",
-            currentTab === "my-tickets" &&
-              "text-transparent bg-clip-text bg-gradient-to-bl from-ss-orange-500/80 to-orange-500/80"
-          )}
-          onClick={() => setCurrenTab("my-tickets")}
-        >
-          My Tickets
-        </button>
-        <button
-          className={clsx(
-            "uppercase",
-            currentTab === "transactions" &&
-              "text-transparent bg-clip-text bg-gradient-to-bl from-ss-orange-500/80 to-orange-500/80"
-          )}
-          onClick={() => setCurrenTab("transactions")}
-        >
-          Transactions
-        </button>
+    <div className="w-full flex flex-col items-center justify-center ">
+      <div className="flex gap-12 text-xl font-semibold mb-8">
+        {TabButton('recent-draws')}
+        {TabButton('my-tickets')}
       </div>
-      <div className="">
-        {currentTab === "statistics" && <Statistics />}
-        {currentTab === "my-tickets" && <MyTickets />}
-        {currentTab === "transactions" && <Transactions />}
-      </div>
+      <AnimatePresence exitBeforeEnter>
+        <motion.div
+          key={currentTab ? currentTab : 'empty'}
+          initial={{ y: 10, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          exit={{ y: -10, opacity: 0 }}
+          transition={{ duration: 0.2 }}
+          className="w-full p-4"
+        >
+          {currentTab === 'recent-draws' && <RecentDraws />}
+          {currentTab === 'my-tickets' && <MyTickets />}
+        </motion.div>
+      </AnimatePresence>
     </div>
   );
 };
