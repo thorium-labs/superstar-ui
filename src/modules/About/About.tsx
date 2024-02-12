35f0e15b760b034c9f6f191241d7bd720416795e
kyar1s <iris.ledesma.27@gmail.com>
2024-02-12 00:24:04 +0000

    feat: mobile re-design about page

--- src/modules/About/About.tsx
+++ src/modules/About/About.tsx
@@ -21,10 +21,14 @@ const About: React.FC = () => {
 
   return (
     <div className="relative">
-      <img src="assets/orange-ball.png" alt="ball" className="h-[3rem] w-[3rem] absolute top-[35rem] left-[-1rem] z-20 animate-floating" />
-      <img src="assets/orange-ball.png" alt="ball" className="h-[4rem] w-[4rem] absolute top-[-3rem] left-[15rem]" />
+      <img src="assets/orange-ball.png" alt="ball" className="h-[3rem] w-[3rem] absolute top-[35rem] left-[-1rem] md:z-20 animate-floating" />
+      <img src="assets/orange-ball.png" alt="ball" className="h-[4rem] w-[4rem] absolute top-[-3rem] left-[15rem] z-20 md:z-0" />
       <img src="assets/orange-ball.png" alt="ball" className="h-[6rem] w-[6rem] absolute bottom-[5rem] left-[4rem] animate-floating" />
-      <img src="assets/orange-ball.png" alt="ball" className="h-[5rem] w-[5rem] absolute top-[5rem] right-[-4rem] z-20 " />
+      <img
+        src="assets/orange-ball.png"
+        alt="ball"
+        className="h-[5rem] w-[5rem] absolute top-[5rem] right-[-1rem] md:top-[5rem] md:right-[-4rem] z-20 "
+      />
       <img src="assets/ss-orange-ball.png" alt="ball" className="h-[9rem] w-[9rem] absolute top-[-6rem] right-[-2rem] animate-floating" />
       <div className="absolute top-[-8rem] left-[-1rem] z-20">
         <img src="assets/launch.png" alt="ball" className="h-[13rem] w-[13rem]  animate-floating " />
@@ -32,7 +36,7 @@ const About: React.FC = () => {
       <img src="assets/ss-orange-ball.png" alt="ball" className="h-[3rem] w-[3rem] absolute bottom-[5rem] right-[5rem] animate-floating" />
       <img src="assets/orange-ball.png" alt="ball" className="h-[7rem] w-[7rem] animate-floating absolute bottom-[7rem] right-[-2rem] z-20" />
       <div className="flex flex-col gap-4 bg-stone-700/20 backdrop-blur rounded-xl relative mt-36 mb-24">
-        <div className="grid grid-cols-2 py-20 px-8 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 py-20 px-4 md:px-8 gap-8">
           <h1 className="text-6xl">What makes us different?</h1>
           <div>
             <p className="text-stone-400 mt-6">
@@ -83,7 +87,7 @@ const About: React.FC = () => {
           </div>
         </div>
         <span className="block w-full h-[2px] bg-gradient-to-bl from-ss-orange-500 to-orange-500 rounded-lg" />
-        <div className="p-4 flex items-center justify-center flex-col mb-24 gap-4">
+        <div className="p-4 flex items-center justify-center flex-col mb-12 md:mb-24 gap-4">
           <h3 className="text-4xl">Let's talk about numbers</h3>
           {/* <p className="max-w-[30rem] text-center text-stone-400">
             <span className="text-ss-orange-500">Super</span>
@@ -91,20 +95,20 @@ const About: React.FC = () => {
           </p> */}
         </div>
         {true ? (
-          <div className="absolute text-stone-100/80 bottom-[-4rem] bg-gradient-to-tr from-ss-orange-500 to-orange-500 flex gap-12 px-12 py-8 rounded-lg left-0 right-0 mx-auto max-w-fit">
+          <div className="absolute text-stone-100/80 bottom-[-2rem] md:bottom-[-4rem] bg-gradient-to-tr from-ss-orange-500 to-orange-500 flex gap-4md:gap-12 px-4 gap-4 md:px-12 py-4 md:py-8 rounded-lg left-0 right-0 mx-auto max-w-fit">
             <div className="flex flex-col items-center justify-center">
-              <h3 className="text-4xl">7</h3>
-              <p className="uppercase font-extrabold text-sm">winners</p>
+              <h3 className="text-xl md:text-4xl">7</h3>
+              <p className="uppercase font-extrabold text-xs md:text-sm">winners</p>
             </div>
             <div className="flex flex-col items-center justify-center">
-              <h3 className="text-4xl">
+              <h3 className="text-xl md:text-4xl">
                 {90} {denom.slice(1)}
               </h3>
-              <p className="uppercase font-extrabold text-sm">Payouts to winners</p>
+              <p className="uppercase font-extrabold text-xs md:text-sm">Payouts to winners</p>
             </div>
             <div className="flex flex-col items-center justify-center">
-              <h3 className="text-4xl">35</h3>
-              <p className="uppercase font-extrabold text-sm">Tickets sold</p>
+              <h3 className="text-xl md:text-4xl">35</h3>
+              <p className="uppercase font-extrabold text-xs md:text-sm">Tickets sold</p>
             </div>
           </div>
         ) : (
