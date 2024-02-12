f2f60f5aaf54cb2a1c1df8983403fe73154becab
kyar1s <iris.ledesma.27@gmail.com>
2024-02-12 01:28:33 +0000

    fix: styles

--- src/components/StarBall/StarBall.tsx
+++ src/components/StarBall/StarBall.tsx
@@ -13,7 +13,7 @@ const StarBall: React.FC<Props> = ({ num, ballColor = 'stone' }) => {
       <img src={`/assets/${ballColor}-ball.png`} className={`md:w-[4rem] w-[2.8rem]`} />
       <Star className={`ball-planet absolute md:w-[4rem] w-[2.8rem] md:h-[4rem] h-[2.8rem]`} />
       <span
-        className={clsx(`absolute text-stone-800 font-bold top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-sm md:font-xl`)}
+        className={clsx(`absolute text-stone-800 font-bold top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-sm md:text-xl`)}
       >
         {num}
       </span>
